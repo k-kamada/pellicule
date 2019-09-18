6fa4b2711d10ef48c16acf1d20d707471cbeab4e
k-kamada <kamadak0404@gmail.com>
2019-09-18 13:29:17 +0900

    Add tokenizer

--- pellicule.c
+++ pellicule.c
@@ -1,5 +1,105 @@
+#include <ctype.h>
+#include <stdarg.h>
+#include <stdbool.h>
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
+
+typedef enum {
+  TK_RESERVED,  // symbol
+  TK_NUM,       // integer
+  TK_EOF,       // end of input
+} TokenKind;
+
+typedef struct Token Token;
+
+struct Token {
+  TokenKind kind; // type of token
+  Token *next;    // next input token
+  int val;        // for TK_NUM: the number itself
+  char *str;      // token strings
+};
+
+// currenly focused token
+Token *token;
+
+// error report function
+void error(char *fmt, ...) {
+  va_list ap;
+  va_start(ap, fmt);
+  vfprintf(stderr, fmt, ap);
+  fprintf(stderr, "\n");
+  exit(1);
+}
+
+// check if next token equals to expected symbol, then advance 
+bool consume(char op) {
+  if (token->kind != TK_RESERVED || token->str[0] != op) {
+    return false;
+  }
+  token = token->next;
+  return true;
+}
+
+// if next token is expected symbol, advance. if not, report error
+bool expect(char op) {
+  if (token->kind != TK_RESERVED || token->str[0] != op) {
+    error("next token is not '%c'", op);
+  }
+  token = token->next;
+}
+
+int expect_number() {
+  if (token->kind != TK_NUM) {
+    error("next token is not a number");
+  }
+  int val = token->val;
+  token = token->next;
+  return val;
+}
+
+bool at_eof() {
+  return token->kind == TK_EOF;
+}
+
+// Generate and set new token next to the current
+Token *new_token(TokenKind kind, Token *cur, char *str) {
+  Token *tok = calloc(1, sizeof(Token));
+  tok->kind = kind;
+  tok->str = str;
+  cur->next = tok;
+  return tok;
+}
+
+Token *tokenize(char *p) {
+  Token head;
+  head.next = NULL;
+  Token *cur = &head;
+
+  while (*p) {
+    // skip white-space
+    if (isspace(*p)) {
+      p++;
+      continue;
+    }
+
+    if (*p == '+' || *p == '-') {
+      cur = new_token(TK_RESERVED, cur, p++);
+      continue;
+    }
+
+    if (isdigit(*p)) {
+      cur = new_token(TK_NUM, cur, p);
+      cur->val = strtol(p, &p, 10);
+      continue;
+    }
+
+    error("Cannot execute tokenize.");
+  }
+
+  new_token(TK_EOF, cur, p);
+  return head.next;
+}
 
 int main(int argc, char **argv) {
   if (argc != 2) {
@@ -7,28 +107,25 @@ int main(int argc, char **argv) {
     return 1;
   }
 
-  char *p = argv[1];
+  token = tokenize(argv[1]);
 
   printf(".intel_syntax noprefix\n");
   printf(".global main\n");
   printf("main:\n");
-  printf("  mov rax, %ld\n", strtol(p, &p, 10));
 
-  while (*p) {
-    if (*p == '+') {
-      p++;
-      printf("  add rax, %ld\n", strtol(p, &p, 10));
+  // expression must start with number
+  printf("  mov rax, %d\n", expect_number());
+
+  // consume operation or number and
+  // generate assembly code
+  while (!at_eof()) {
+    if (consume('+')) {
+      printf("  add rax, %d\n", expect_number());
       continue;
     }
 
-    if (*p == '-') {
-      p++;
-      printf("  sub rax, %ld\n", strtol(p, &p, 10));
-      continue;
-    }
-
-    fprintf(stderr, "Unexpected character: '%c'\n", *p);
-    return 1;
+    expect('-');
+    printf("  sub rax, %d\n", expect_number());
   }
 
   printf("  ret\n");
